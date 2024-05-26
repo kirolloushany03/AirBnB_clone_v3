de3ec2c22e90d640f8dd3e440dcc8d950ad7f806
root <root@fourthpyramid.localdomain>
2024-05-26 20:18:45 +0300

    fifth task

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ this module sets to flask web application"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -15,6 +15,13 @@ def teardown(exception):
     """this method call storage.close()"""
     storage.close()
 
+
+@app.errorhandler(404)
+def error_404(error):
+    """error handeling for error 404 not found page"""
+    response = jsonify({"error": "Not found"})
+    response.status_code = 404
+    return response
 
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
