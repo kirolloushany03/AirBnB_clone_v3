96b49c98278a7da3e0e20ac41e94155105ac92c3
ali-hy <ali.haitham.ymy@gmail.com>
2024-05-26 19:55:44 +0300

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,3 +8,22 @@ from api.v1.views import app_views
 def status():
     """an this return json data to check the status"""
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats")
+def stats():
+    """return the count of all classes"""
+    from models import storage
+    from models.state import State
+    from models.city import City
+    from models.user import User
+    from models.place import Place
+    from models.review import Review
+    from models.amenity import Amenity
+
+    classes = {"Amenity": Amenity,
+               "City": City,
+               "Place": Place,
+               "Review": Review,
+               "State": State,
+               "User": User}
+    return jsonify({key: storage.count(value) for key, value in classes.items()})
