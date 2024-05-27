1c624537e589448d7c9f90d35462b29b50031adb
ali-hy <ali.haitham.ymy@gmail.com>
2024-05-27 23:33:47 +0300

    another attempt

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,9 +5,9 @@ from flask import Blueprint
 app_views = Blueprint("views_blue", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
-from api.v1.views import states
-from api.v1.views import cities
-from api.v1.views import amenities
-from api.v1.views import users
-from api.v1.views import places
-from api.v1.views import places_reviews
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
