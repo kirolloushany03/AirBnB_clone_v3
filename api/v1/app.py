bf1c06685b2ad7c31ea64400e5368dee1be5d4eb
ali-hy <ali.haitham.ymy@gmail.com>
2024-05-27 22:24:41 +0300

    try fixing app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -26,6 +26,5 @@ def error_404(error):
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = int(os.getenv("HBNB_API_PORT", "5000"))
-    app.url_map.strict_slashes = False
 
     app.run(host=host, port=port, threaded=True)
