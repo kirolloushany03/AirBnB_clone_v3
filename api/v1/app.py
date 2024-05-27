c872bf6fd99fc2cd7535e83bb63edea7ad226a0e
ali-hy <ali.haitham.ymy@gmail.com>
2024-05-27 23:41:46 +0300

    12

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ this module initializes the Flask web application"""
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 import os
@@ -8,6 +9,8 @@ import os
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 app.register_blueprint(app_views)
 
 
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """setup blueprint for flaskk"""
 from flask import Blueprint
-
 app_views = Blueprint("views_blue", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
